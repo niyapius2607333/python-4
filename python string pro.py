34c6a8d02921a848c8b255adeecb089185071260
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-08 14:47:35 +0530

    Add files via upload

---
+++ python string pro.py
@@ -0,0 +1,12 @@
+'''
+Author : Niya Pius
+Date : 08-10-2024
+Python program to concatenate and extract a string
+Version 1.0
+'''
+str1=("Niya")
+str2=("Pius")
+str3=str1+" "+str2
+print(str3)
+str4=str3[4:10]
+print(str4)
