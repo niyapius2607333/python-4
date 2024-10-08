9ebf7bb93060050ce88c19c64adf0661a32b7ed7
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-08 14:47:02 +0530

    Add files via upload

---
+++ python operators 1.py
@@ -0,0 +1,22 @@
+'''
+Author : Niya Pius
+Date : 08-10-2024
+Python program to do arithmetic operations
+Version 1.0
+'''
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+num3=int(input("Enter a third number:"))
+sum =num1 + num2
+print("The sum of num1 and num2 is :",sum)
+sub=num2-num1
+print("The difference when num2 is subtracted from num1 is:",sub)
+multi=num1*num2
+print("The product of num1 and num2 is:,",multi)
+div=num1/num2
+print("The quotient when num1 is divided by num2:",div)
+remain=num1%num2
+print("The remainder when num1 is divided by num2 is:",remain)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is :",result)
+
