ee9f067199e805618f440894bede92e28df73583
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-08 15:02:42 +0530

    Add files via upload

--- python string pro.py
+++ python string pro.py
@@ -4,9 +4,10 @@ Date : 08-10-2024
 Python program to concatenate and extract a string
 Version 1.0
 '''
-str1=("Niya")
-str2=("Pius")
+str1=input("Enter your first name:")
+str2=input("Enter your last name:")
 str3=str1+" "+str2
 print(str3)
-str4=str3[4:10]
-print(str4)
+length=len(str1)
+print("First name=",str3[:length])
+
