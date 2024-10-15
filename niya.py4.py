256ec4325cb2c98b4237b04ea0c117dd58c21b3a
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-15 15:23:56 +0530

    Add files via upload

---
+++ niya.py4.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+small=0
+if a>b:
+    small=b
+elif small<c:
+    print(c,"is the smallest")
+else :
+    print(small,"is the smallest")
+
