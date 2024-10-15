88aa9e265b5ccad8694b316e39111f4941ae3e61
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-15 14:25:10 +0530

    niya.py1.py

--- niya.py1.py
+++ niya.py1.py
@@ -1,3 +1,9 @@
+'''
+Author : Niya Pius
+Date : 15-10-2024
+Python program to determine a number is positive of not
+Version 1.0
+'''
 number=int(input("Enter a number"))
 if number>0:
    print("The Given Number:",number,"is positive")
