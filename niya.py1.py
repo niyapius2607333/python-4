a83f97df3d09351b05534755b40b5950b69c6551
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-15 14:23:01 +0530

    Add files via upload

---
+++ niya.py1.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a number"))
+if number>0:
+   print("The Given Number:",number,"is positive")
+elif number<0:
+    print("The given number:",number,"is negative")
+else:
+    print("THe given number",number,"is zero")
