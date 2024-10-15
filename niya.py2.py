0d46588458b4e68762c10d4254f8991e25fafbf1
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-15 14:44:27 +0530

    niya.py2.py

--- niya.py2.py
+++ niya.py2.py
@@ -1,3 +1,9 @@
+'''
+Author : Niya Pius
+Date : 15-10-2024
+Python program to find eligible for driving license
+Version 1.0
+'''
 age=int(input("Enter your age"))
 if age>=16:
     print("ELIGIBLE FOR A DRIVING LICENSE")
